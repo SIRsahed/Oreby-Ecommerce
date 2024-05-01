be43de58169a3de26adcf8abd8707835658dbdf2
Sahed Rahman <sahedrahmanshakil@gmail.com>
2024-05-02 00:10:36 +0600

    demo

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+import Header from "./components/Header"
+
 function App() {
 
   return (
     <>
-      <h2>Hello World</h2>
+      <Header/>
     </>
   )
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from 'react'
+import Container from './Container'
+import Flex from './Flex'
+import Logo from "../assets/logo.png"
+import { RxCross2 } from "react-icons/rx";
+import { FaBars } from "react-icons/fa";
+
+const Header = () => {
+  let [show, setShow] = useState(false)
+  return (
+    <>
+      <nav className='lg:h-[80px]'>
+        <Container>
+          <Flex className="justify-between lg:h-[80px] items-center">
+            <div className="w-1/3 items-center">
+              <img src={Logo} alt="logo" />
+            </div>
+            <div className="w-2/3">
+              <ul className={`lg:flex lg:flex-row flex flex-col gap-y-3 gap-x-10 items-center font-sans text-[#767676] text-[18px] font-medium font-normal lg:static fixed duration-[800ms] ${show == true ? "bg-[#262626] top-[20px] left-0 w-full text-center py-2" : "top-[20px] left-[-100px]"}`}>
+                <li><a className='lg:hover:text-[#262626] hover:bg-[#fff] rounded-sm' href="#">Home</a></li>
+                <li><a className='lg:hover:text-[#262626] hover:bg-[#fff] rounded-sm' href="#">Shop</a></li>
+                <li><a className='lg:hover:text-[#262626] hover:bg-[#fff] rounded-sm' href="#">About</a></li>
+                <li><a className='lg:hover:text-[#262626] hover:bg-[#fff] rounded-sm' href="#">Contacts</a></li>
+                <li><a className='lg:hover:text-[#262626] hover:bg-[#fff] rounded-sm' href="#">Journal</a></li>
+              </ul>
+            </div>
+            <div className="lg:hidden cursor-pointer" onClick={() => setShow(!show)}>
+              {show == true ? <RxCross2 /> : <FaBars />}
+            </div>
+          </Flex>
+        </Container>
+      </nav>
+    </>
+  )
+}
+
+export default Header
